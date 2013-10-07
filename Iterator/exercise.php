301fe55f86fc4f7c7af5ae5ce36e02aaef04920a
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2013-10-07 11:35:39 -0400

    Implement iterator exercise.

--- Iterator/exercise.php
+++ Iterator/exercise.php
@@ -6,33 +6,22 @@
  * @see    http://en.wikipedia.org/wiki/Fibonacci_number
  */
 
-class Fibonacci implements Iterator {
-    protected $value = 0;
-    protected $sum = 0;
-    protected $key = 0;
-    public function rewind() {
-        $this->value = 0;
-        $this->key   = 0;
-    }
-    public function current() {
-        return $this->value;
-    }
-    public function key() {
-        return $this->key;
-    }
-    public function next() {
-        // @todo here the code to implement
-    }
-    
-    public function valid() {
-        return true;
+function fibonacci()
+{
+    for($a = 0, $b = 1; ;) {
+        yield $a;
+        $c = $a;
+        $a += $b;
+        $b = $c;
     }
 }
 
 // check the first 10 Fibonacci's numbers
-$num = new Fibonacci();
 $correct = array(0, 1, 1, 2, 3, 5, 8, 13, 21, 34);
-for ($i = 0; $i < 10; $i++) {
-    printf ("%s<br>", $num->current() === $correct[$i] ? 'OK ' : 'ERROR');
-    $num->next();
+foreach (fibonacci() as $i => $num) {
+    if ($i == 10) {
+        break;
+    }
+
+    printf ("%i%s<br>", $num, $num === $correct[$i] ? 'OK ' : 'ERROR');
 }
